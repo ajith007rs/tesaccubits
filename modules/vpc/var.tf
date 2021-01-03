20589afcaf3cadba91cdf974f0bf7c3987051347
Ajith R S <ajith.r-s@ovhcloud.com>
2021-01-03 15:05:02 +0530

    Added VPC

---
+++ modules/vpc/var.tf
@@ -0,0 +1,18 @@
+variable "vpc_cidr" {
+    type = string
+}
+
+variable "vpc_tenancy" {
+    type    = string
+    default = "default"
+}
+
+variable "vpc_dns_support" {
+    type    = bool
+    default = "true"
+}
+
+variable "vpc_dns_hostnames" {
+    type    = bool
+    default = "true"
+}
---
+++ modules/vpc/vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "rs_vpc" {
+    cidr_block           = var.vpc_cidr
+    instance_tenancy     = var.vpc_tenancy
+    enable_dns_support   = var.vpc_dns_support
+    enable_dns_hostnames = var.vpc_dns_hostnames
+
+}
+
+output "out_vpc_id" {
+    value = aws_vpc.rs_vpc.id
+}   
